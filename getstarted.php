993c19370fa0aa4229d2d975609b9bedb5a9349e
drgreatwonder <muoghaluchidinma@gmail.com>
2019-09-24 10:35:17 +0100

    fresh commit

--- getstarted.php
+++ getstarted.php
@@ -29,34 +29,34 @@
       <div class="row mb-3">
         <div class="col-md-6 col-sm-12">
           <label>First Name</label>
-          <input type="text" class="form-control" name="firstname" placeholder="Enter First Name">
+          <input type="text" class="form-control" name="firstname" placeholder="Enter First Name" required/>
         </div>
 
         <div class="col-md-6 col-sm-12">
           <label>Last Name</label>
-          <input type="text" class="form-control" name="lastname" placeholder="Enter Last Name">
+          <input type="text" class="form-control" name="lastname" placeholder="Enter Last Name" required/>
         </div>
       </div>
 
       <div class="row mb-3">
         <div class="col-md-6 col-sm-12">
           <label>Username</label>
-          <input type="text" class="form-control" name="username" placeholder="Enter Preffered Username">
+          <input type="text" class="form-control" name="username" placeholder="Enter Preffered Username" required/>
         </div>
         <div class="col-md-6 col-sm-12">
           <label>Date Of Birth</label>
-          <input type="date" class="form-control" name="dob">
+          <input type="date" class="form-control" name="dob" required/>
         </div>
       </div>
 
       <div class="row mb-3">
         <div class="col-md-6 col-sm-12">
           <label>Email address</label>
-          <input type="email" class="form-control" name="email" placeholder="Enter email">
+          <input type="email" class="form-control" name="email" placeholder="Enter email" required/>
         </div>
         <div class="col-md-6 col-sm-12">
           <label>Confirm Email address</label>
-          <input type="email" class="form-control" name="email1" placeholder="Enter email">
+          <input type="email" class="form-control" name="email1" placeholder="Enter email" required/>
         </div>
       </div>
 
@@ -64,18 +64,18 @@
       <div class="row mb-3">
         <div class="col-md-6 col-sm-12">
           <label>Password</label>
-          <input type="password" class="form-control" name="password" placeholder="Password">
+          <input type="password" class="form-control" name="password" placeholder="Password" required/>
         </div>
         <div class="col-md-6 col-sm-12">
           <label>Confirm Password</label>
-          <input type="password" class="form-control" name="password1" placeholder="Confirm Password">
+          <input type="password" class="form-control" name="password1" placeholder="Confirm Password" required/>
         </div>
       </div>
 
       <div class="row mb-3">
         <div class="col-md-6 col-sm-12">
           <label>Most Preferred Language</label>
-          <select class="form-control" name="most_preferred_language">
+          <select class="form-control" name="most_preferred_language" required>
             <option>Take your pick</option>
             <option>HTML</option>
             <option>CSS</option>
@@ -95,7 +95,7 @@
         <div class="col-md-6 col-sm-12">
           <label>About Me</label>
           <div class="form-group">
-            <textarea class="form-control rounded-0" name="bio" rows="5" placeholder=""></textarea>
+            <textarea class="form-control rounded-0" name="bio" rows="5" placeholder="" required></textarea>
           </div>
         </div>
       </div>
